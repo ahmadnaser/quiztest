839bd56958668d6d89950e4c6e0f2328db5221ff
omuji <mtkcomj@gmail.com>
2017-03-05 11:16:17 +0900

    うんちく

---
+++ Assets/UserDataController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserDataController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void UserDataReceived(int userId,string userName,string createtaime,int totalanswer,int correctanswer){
+		textmaa (userId);
+
+	}
+
+	//自分の名前のUIを更新するメソッド
+	public void textmaa(int userId){
+		//gamebjectを取得
+
+
+		//textに代入
+		//text = userId;
+}
+}
