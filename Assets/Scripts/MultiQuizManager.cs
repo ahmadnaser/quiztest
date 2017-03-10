61abe4465287b01c268a0c4c92973a7d9985ed8e
DESKTOP-TNPO3RF\瀬戸敬晴 <trike1236@gmail.com>
2017-03-11 02:33:31 +0900

    test

--- Assets/Scripts/MultiQuizManager.cs
+++ Assets/Scripts/MultiQuizManager.cs
@@ -1,25 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MultiQuizManager : Photon.PunBehaviour {
 
     PhotonView photonView;
+
+    float myTime;
+    float otherTime;
     void Start()
     {
         photonView = GetComponent<PhotonView>();
     }
 
     void SendAllAnswerTime(float time) {
-        float myTime = time;
-        photonView.RPC("SendTimeRPC", PhotonTargets.Others,time);
+        myTime = time;
+        photonView.RPC("SendTimeRPC", PhotonTargets.Others,time,isFasterThan());
 
     }
 
     [PunRPC]
-    private void SendTimeRPC(float time)
+    private void SendTimeRPC(float time,UnityAction<bool> callback)
     {
-
+        otherTime = time;
     }
     public bool isFasterThan()
     {
